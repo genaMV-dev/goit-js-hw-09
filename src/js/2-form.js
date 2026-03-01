a8d7f5dd5240106ac56ec1db97af36039a4d6181
Ign1s123 <genamisinskij@gmail.com>
2026-03-01 21:23:51 +0200

    #

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,29 +1,22 @@
 const STORAGE_KEY = "feedback-form-state";
-
 const form = document.querySelector(".feedback-form");
-
 
 let formData = {
   email: "",
   message: "",
 };
 
+const savedData = localStorage.getItem(STORAGE_KEY);
 
-document.addEventListener("DOMContentLoaded", () => {
-  const savedData = localStorage.getItem(STORAGE_KEY);
-
-  if (!savedData) return;
-
+if (savedData) {
   try {
     formData = JSON.parse(savedData);
-
     form.elements.email.value = formData.email || "";
     form.elements.message.value = formData.message || "";
   } catch (error) {
     console.error("Invalid data in localStorage");
   }
-});
-
+}
 
 form.addEventListener("input", (e) => {
   const { name, value } = e.target;
@@ -34,7 +27,6 @@ form.addEventListener("input", (e) => {
 
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 });
-
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -48,15 +40,12 @@ form.addEventListener("submit", (e) => {
 
   console.log(formData);
 
-  
   localStorage.removeItem(STORAGE_KEY);
 
-  
   formData = {
     email: "",
     message: "",
   };
 
-  
   form.reset();
 });
